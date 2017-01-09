bde9c20a87f033b9e8fa189001c5677384bac3f9
Maxiwell Garcia <maxiwell@gmail.com>
2017-01-09 20:22:17 -0200

    [Santander] Remove unnecessary functions

--- ofx-santander.py
+++ ofx-santander.py
@@ -93,32 +93,6 @@ class Santander():
         driver.find_element_by_link_text("confirmar").click()
         sleep(5)
 
-
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
-        return True
-   
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException, e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
 if __name__ == "__main__":
     santander = Santander()
     try:
